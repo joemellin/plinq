c727b6e92be6fb80f26bded47dc3bdf0850cab39
Josh Schwartzman <geeosh@gmail.com>
2012-12-01 22:26:51 +0100

    Fix for mongoid on heroku

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,6 +1,6 @@
 class PlayController < ApplicationController
   def index
     @songs = Song.asc(:created_at).limit(20)
-    @song = @songs.last
+    @song = @songs.last if @songs.present?
   end
 end
